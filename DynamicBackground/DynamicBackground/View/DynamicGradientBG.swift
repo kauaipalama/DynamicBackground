f742d2b9e0865ada236994e27a5653ee954e937c
kauaipalama <kauaipalama@gmail.com>
2020-12-12 14:34:10 -0800

    Experimenting with setting gradient.colors trying to store a computed property containing color references. Locations set for first test gradient.

--- DynamicBackground/DynamicBackground/View/DynamicGradientBG.swift
+++ DynamicBackground/DynamicBackground/View/DynamicGradientBG.swift
@@ -16,8 +16,8 @@ protocol DynamicGradientBG: UIViewController {
 extension DynamicGradientBG {
     func setupGradientLayer() {
         let gradient = CAGradientLayer()
-        gradient.colors = [UIColor.systemPink.cgColor, UIColor.orange.cgColor, UIColor.red.cgColor, UIColor.purple.cgColor, UIColor.black.cgColor]
-        gradient.locations = [0.0, 0.005, 0.06, 0.24, 0.88]
+        gradient.colors = [UIColor(red: 43, green: 49, blue: 96, alpha: 100), UIColor.orange.cgColor, UIColor.red.cgColor, UIColor(red: 43, green: 49, blue: 96, alpha: 100).cgColor]
+        gradient.locations = [0.0, 0.005, 0.06, 0.24]
         gradient.startPoint = CGPoint(x: 0, y: 1.0)
         gradient.endPoint = CGPoint(x: 0, y: 0)
         gradient.frame = CGRect(x: 0.0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
@@ -27,4 +27,11 @@ extension DynamicGradientBG {
     func animateGradientLayer() {
         
     }
+    
+//    var solarColors1 = [CGColor(red: 43, green: 49, blue: 96, alpha: 100), CGColor(red: 55, green: 61, blue: 110, alpha: 100), CGColor(red: 95, green: 95, blue: 138, alpha: 100), CGColor(red: 255, green: 166, blue: 47, alpha: 100)]
 }
+
+//
+//    gradient.locations = [0.0, 0.252, 0.573, 1.0]
+
+
